49d1ac6103bf35ed31b7032bb5275d87f469fb41
Luis Augusto Peña Pereira <lpenap@gmail.com>
2021-02-05 16:21:09 -0300

    Add a cache (DB) system avaiable to commands
    Changes:
    * use of SleekDB to implement a simple nosql cache
    * commands are able to insert, get and update arrays
      in the form of a key,value (values must be arrays
      representing the data you want to store).

--- src/Alassea/Commands/System/QodCommand.php
+++ src/Alassea/Commands/System/QodCommand.php
@@ -5,52 +5,88 @@ namespace Alassea\Commands\System;
 use Alassea\Commands\AbstractCommand;
 use Discord\Parts\Channel\Message;
 use Discord\Parts\Embed\Embed;
+use Alassea\Database\Cache;
 
 class QodCommand extends AbstractCommand {
 	protected $url = 'https://quotes.rest/qod.json?category=';
-	protected $default = 'funny';
+	protected $defaultCategory = 'funny';
+	protected $category = null;
 	public function run($params) {
 		$text = null;
 		$qodResponse = $this->getQod ();
 		$embed = null;
+
 		if (isset ( $qodResponse->contents->quotes [0] )) {
-			$quote = $qodResponse->contents->quotes [0];
-			$text = 'Here is your \'' . $quote->title . '\'';
-			$embed = $this->getDiscord ()->factory ( Embed::class, [ 
-					"title" => $quote->quote,
-					"description" => 'Tags: ' . implode ( ", ", $quote->tags ),
-					'color' => '#0099ff',
-					"thumbnail" => [ 
-							"url" => $quote->background,
-							"height" => 20,
-							"width" => 20
-					],
-					"author" => [ 
-							'name' => $quote->author
-					],
-					"footer" => [ 
-							'text' => 'They Said So(R), ' . $qodResponse->copyright->url,
-							'icon_url' => 'https://theysaidso.com/branding/theysaidso.png'
-					]
-			], true );
+			$text = 'Here is your \'' . $qodResponse->contents->quotes [0]->title . '\'';
 		} else {
-			$text = 'Oops!, the qotd service is down (or the selected category doesn\'t exist)!';
+			$text = 'Oops!, the qotd service is down (or the selected category doesn\'t exist)! Nevermind, here is my default quote!:';
+			$qodResponse = $this->getDefaultQuote ();
+		}
+		$quote = $qodResponse->contents->quotes [0];
+		$embed = $this->getDiscord ()->factory ( Embed::class, [ 
+				"title" => $quote->quote,
+				"description" => 'Tags: ' . implode ( ", ", $quote->tags ),
+				'color' => '#0099ff',
+				"thumbnail" => [ 
+						"url" => $quote->background,
+						"height" => 20,
+						"width" => 20
+				],
+				"author" => [ 
+						'name' => $quote->author
+				]
+		], true );
+		if (isset ( $qodResponse->copyright )) {
+			$embed->setFooter ( 'They Said So(R), ' . $qodResponse->copyright->url, 'https://theysaidso.com/branding/theysaidso.png' );
 		}
 		$message = $this->getMessage ();
 		$message->channel->sendMessage ( "{$message->author}, {$text}", false, $embed )->then ( function (Message $message) {
-			echo 'Message sent!' . PHP_EOL;
+			echo '\nMessage sent!' . PHP_EOL;
 		} )->otherwise ( function (\Exception $e) {
-			echo 'Error sending message: ' . $e->getMessage () . PHP_EOL;
+			echo '\nError sending message: ' . $e->getMessage () . PHP_EOL;
 		} );
 	}
 	public function prepare($params) {
 		if (isset ( $params [0] )) {
-			$this->url .= strtolower ( $params [0] );
+			$this->category = strtolower ( $params [0] );
+			$this->url .= $this->category;
 		} else {
-			$this->url .= $this->default;
+			$this->url .= $this->defaultCategory;
+			$this->category = $this->defaultCategory;
 		}
 	}
 	public function getQod() {
-		return json_decode ( file_get_contents ( $this->url ) );
+		$key = date ( "Y-m-d" ) . $this->category;
+		return $this->getBot ()->getCache ()->get ( $key, function ($myQod) use ($key) {
+			if ($myQod == null) {
+				echo "Fetching new QoD";
+				$contents = file_get_contents ( $this->url );
+				$array = json_decode ( $contents, true );
+				if ($array != null) {
+					$myQod = $this->getBot ()->getCache ()->insert ( $key, $array, "qod" );
+				}
+			} else {
+				echo "Found QoD in chache!, returning";
+			}
+			return json_decode ( json_encode ( $myQod ) );
+		}, "qod" );
+	}
+	private function getDefaultQuote($asJsonObj = true) {
+		$quote = array (
+				'contents' => array (
+						'quotes' => array (
+								array (
+										'title' => 'SciFi quote of the day',
+										'quote' => 'The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.',
+										'background' => 'https://upload.wikimedia.org/wikipedia/commons/3/34/Isaac.Asimov01.jpg',
+										'author' => 'Isaac Asimov',
+										'tags' => array (
+												'default'
+										)
+								)
+						)
+				)
+		);
+		return $asJsonObj ? json_decode ( json_encode ( $quote ) ) : $quote;
 	}
 }
