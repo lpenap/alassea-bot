568585d681a549a181bed73dc6aaefa77de01964
Luis Augusto Peña Pereira <lpenap@gmail.com>
2021-02-13 11:33:33 -0300

    Fix command manager and logger reference

--- src/Alassea/Events/MessageCreateEventHandler.php
+++ src/Alassea/Events/MessageCreateEventHandler.php
@@ -2,22 +2,25 @@
 
 namespace Alassea\Events;
 
+use Alassea\Commands\CommandManager;
+
 class MessageCreateEventHandler extends AbstractEventHandler {
 	public function handle(...$args) {
 		if (! isset ( $args [0] ) || $args [0] == null) {
-			$this->bot->getLogger ()->warning ( "Received a null message? this maybe a problem..." );
+			$this->logger->warning ( "Received a null message? this maybe a problem..." );
 			return;
 		}
 		$message = $args [0];
-		if ( $message->content !== "" && $message->content [0] == $this->bot->getPrefix ()) {
-			$this->bot->getLogger ()->debug ( "Message received: ", [ 
+		$prefix = $this->bot->getPrefs ()->get ( 'prefix' );
+		if ($message->content !== "" && $message->content [0] == $prefix) {
+			$this->logger->debug ( "Message received: ", [ 
 					"author" => $message->author->username,
 					"msg" => $message->content
 			] );
 			$content = preg_replace ( "/\s+/", " ", strtolower ( $message->content ) );
 			$params = explode ( " ", $content );
-			$cmd = ltrim ( array_shift ( $params ), $this->bot->getPrefix () );
-			$this->bot->executeCommand ( $cmd, $params, $message );
+			$cmd = ltrim ( array_shift ( $params ), $prefix );
+			CommandManager::instance ()->executeCommand ( $cmd, $params, $message );
 		}
 	}
 }
