45615ebdd2f52870f4127ba6de6c50f2ebcbc787
Luis Augusto Peña Pereira <lpenap@gmail.com>
2021-02-13 11:34:10 -0300

    Add logger creation in abstract event handler

--- src/Alassea/Events/AbstractEventHandler.php
+++ src/Alassea/Events/AbstractEventHandler.php
@@ -4,14 +4,19 @@ namespace Alassea\Events;
 
 use Alassea\Alassea;
 use Discord\Discord;
+use Monolog\Logger as Monolog;
+use Monolog\Handler\StreamHandler;
 
 abstract class AbstractEventHandler implements EventHandlerInterface {
 	protected $discord;
 	protected $bot;
+	protected $logger;
 	public function setup($event, Discord $discord, Alassea $bot) {
 		$this->bot = $bot;
 		$this->discord = $discord;
 		$this->discord->on ( $event, $this );
+		$this->logger = new Monolog ( get_class ( $this ) );
+		$this->logger->pushHandler ( new StreamHandler ( 'php://stdout', $this->bot->getPrefs ()->get ( 'log_level' ) ) );
 	}
 	public function __invoke(...$args) {
 		$this->handle ( ...$args );
