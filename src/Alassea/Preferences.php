32b8e7e35c60b8ff625b2e7d707c4257175de564
Luis Augusto Peña Pereira <lpenap@gmail.com>
2021-05-05 13:20:04 -0300

    Increase version number

--- src/Alassea/Preferences.php
+++ src/Alassea/Preferences.php
@@ -5,7 +5,7 @@ namespace Alassea;
 use Monolog\Logger as Monolog;
 
 class Preferences {
-	public const VERSION = "0.10";
+	public const VERSION = "0.11";
 	// Namespaces must be relative to Alassea\\Commands root namespace
 	// which is where the CommandManager is located.
 	// Also, do not include the trailing \\ in the relative namespace.
